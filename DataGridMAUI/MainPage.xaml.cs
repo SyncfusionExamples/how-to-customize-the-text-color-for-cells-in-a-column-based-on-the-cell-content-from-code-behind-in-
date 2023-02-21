a6aec400267c78b04583c9d7c5dbf04877b76502
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2023-02-21 10:05:41 +0530

    addedsample and KB file

---
+++ DataGridMAUI/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Syncfusion.Maui.DataGrid;
+
+namespace DataGridMAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        Style style = new Style(typeof(DataGridCell));
+        style.Setters.Add(new Setter() { Property = DataGridCell.TextColorProperty, Value = new Binding("Freight", BindingMode.TwoWay, new StyleConverter()) });
+        dataGrid.Columns[2].CellStyle = style;
+    }
+}
+
---
+++ DataGridMAUI/ViewModel/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridMAUI
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ViewModel()
+        {
+            SetRowstoGenerate(200);
+        }
+
+        #region ItemsSource
+
+        private ObservableCollection<OrderInfo> ordersInfo;
+
+        public ObservableCollection<OrderInfo> OrdersInfo
+        {
+            get { return ordersInfo; }
+            set { this.ordersInfo = value; }
+        }
+
+        #endregion
+
+        #region ItemSource Generator
+
+        public void SetRowstoGenerate(int count)
+        {
+            OrderInfoRepository order = new OrderInfoRepository();
+            ordersInfo = order.GetOrderDetails(100);
+        }
+
+        #endregion
+    }
+}
